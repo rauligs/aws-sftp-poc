da45726b37e56531b3c97cc583a3013a49cea398
Raul Garcia <raul.garcia-suarez@worldpay.com>
2019-11-26 16:58:07 +0000

    Add sftp endpoint as output

---
+++ terraform/output.tf
@@ -0,0 +1,11 @@
+output "bucket_name" {
+  value = aws_s3_bucket.poc-sftp-s3-bucket.id
+}
+
+output "sftp_id" {
+  value = aws_transfer_server.sftp-poc.id
+}
+
+output "sftp_endpoint" {
+  value = aws_transfer_server.sftp-poc.endpoint
+}
